a92b2f0feabeab56f06ac0c8875ed6fbecfc8752
eswaldots <aaronvendedor@gmail.com>
2025-05-15 10:18:52 -0400

    feat: implemented lookup necessary for officials crud

--- infrastructure/web/src/controllers/lookup.rs
+++ infrastructure/web/src/controllers/lookup.rs
@@ -1,137 +1,152 @@
+use crate::Result;
 use application::dtos::lookup::CreateBasicLookUpDTO;
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
 use domain::{
-    entities::{brigade, division, organism},
+    entities::{brigade, charge, division, hierarchy, organism},
     repositories::lookup_repository::LookupRepository,
 };
 use tracing::{debug, error};
 
 use crate::state::AppState;
 
-pub async fn get_brigades(State(app_state): State<AppState>) -> impl IntoResponse {
-    match app_state
+pub async fn get_brigades(State(app_state): State<AppState>) -> Result<Response> {
+    let brigades = app_state
         .lookup_repository
         .find::<brigade::Entity, brigade::Model, brigade::ActiveModel>()
-        .await
-    {
-        Ok(brigades) => {
-            debug!("{:?}", Json(&brigades));
+        .await?;
 
-            (StatusCode::OK, Json(brigades)).into_response()
-        }
-        Err(e) => {
-            error!("Error getting brigades: {}", e);
-
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
-    }
+    debug!("{:?}", Json(&brigades));
+    Ok((StatusCode::OK, Json(brigades)).into_response())
 }
 
 pub async fn create_brigade(
     State(app_state): State<AppState>,
     Json(brigade): Json<CreateBasicLookUpDTO>,
-) -> impl IntoResponse {
+) -> Result<Response> {
     let active_model = brigade::ActiveModel {
         id: Default::default(),
         name: sea_orm::Set(brigade.name),
     };
 
-    match app_state
+    app_state
         .lookup_repository
         .create::<brigade::Entity, brigade::Model, brigade::ActiveModel>(active_model)
-        .await
-    {
-        Ok(_) => StatusCode::CREATED.into_response(),
-        Err(e) => {
-            error!("Error creating brigade: {}", e);
+        .await?;
 
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
-    }
+    Ok(StatusCode::CREATED.into_response())
 }
 
-pub async fn get_organisms(State(app_state): State<AppState>) -> impl IntoResponse {
-    match app_state
+pub async fn get_organisms(State(app_state): State<AppState>) -> Result<Response> {
+    let organisms = app_state
         .lookup_repository
         .find::<organism::Entity, organism::Model, organism::ActiveModel>()
-        .await
-    {
-        Ok(organisms) => {
-            debug!("{:?}", Json(&organisms));
+        .await?;
 
-            (StatusCode::OK, Json(organisms)).into_response()
-        }
-        Err(e) => {
-            error!("Error getting organisms: {}", e);
-
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
-    }
+    debug!("{:?}", Json(&organisms));
+    Ok((StatusCode::OK, Json(organisms)).into_response())
 }
 
 pub async fn create_organism(
     State(app_state): State<AppState>,
     Json(organism): Json<CreateBasicLookUpDTO>,
-) -> impl IntoResponse {
+) -> Result<Response> {
     let active_model = organism::ActiveModel {
         id: Default::default(),
         name: sea_orm::Set(organism.name),
         phone: sea_orm::Set(String::new()), // Default empty string for phone
     };
 
-    match app_state
+    app_state
         .lookup_repository
         .create::<organism::Entity, organism::Model, organism::ActiveModel>(active_model)
-        .await
-    {
-        Ok(_) => StatusCode::CREATED.into_response(),
-        Err(e) => {
-            error!("Error creating organism: {}", e);
+        .await?;
 
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
-    }
+    Ok(StatusCode::CREATED.into_response())
 }
 
-pub async fn get_divisions(State(app_state): State<AppState>) -> impl IntoResponse {
-    match app_state
+pub async fn get_divisions(State(app_state): State<AppState>) -> Result<Response> {
+    let divisions = app_state
         .lookup_repository
         .find::<division::Entity, division::Model, division::ActiveModel>()
-        .await
-    {
-        Ok(divisions) => {
-            debug!("{:?}", Json(&divisions));
+        .await?;
 
-            (StatusCode::OK, Json(divisions)).into_response()
-        }
-        Err(e) => {
-            error!("Error getting divisions: {}", e);
-
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
-    }
+    debug!("{:?}", Json(&divisions));
+    Ok((StatusCode::OK, Json(divisions)).into_response())
 }
 
 pub async fn create_division(
     State(app_state): State<AppState>,
     Json(division): Json<CreateBasicLookUpDTO>,
-) -> impl IntoResponse {
+) -> Result<Response> {
     let active_model = division::ActiveModel {
         id: Default::default(),
         name: sea_orm::Set(division.name),
         state: sea_orm::Set(String::new()), // Default empty string for state
     };
 
-    match app_state
+    app_state
         .lookup_repository
         .create::<division::Entity, division::Model, division::ActiveModel>(active_model)
-        .await
-    {
-        Ok(_) => StatusCode::CREATED.into_response(),
-        Err(e) => {
-            error!("Error creating division: {}", e);
+        .await?;
 
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
-    }
+    Ok(StatusCode::CREATED.into_response())
+}
+
+pub async fn get_charges(State(app_state): State<AppState>) -> Result<Response> {
+    let charges = app_state
+        .lookup_repository
+        .find::<charge::Entity, charge::Model, charge::ActiveModel>()
+        .await?;
+
+    debug!("{:?}", Json(&charges));
+    Ok((StatusCode::OK, Json(charges)).into_response())
+}
+
+pub async fn create_charge(
+    State(app_state): State<AppState>,
+    Json(charge): Json<CreateBasicLookUpDTO>,
+) -> Result<Response> {
+    let active_model = charge::ActiveModel {
+        id: Default::default(),
+        name: sea_orm::Set(charge.name),
+    };
+
+    app_state
+        .lookup_repository
+        .create::<charge::Entity, charge::Model, charge::ActiveModel>(active_model)
+        .await?;
+
+    Ok(StatusCode::CREATED.into_response())
+}
+
+pub async fn get_hierarchies(State(app_state): State<AppState>) -> Result<Response> {
+    let hierarchies = app_state
+        .lookup_repository
+        .find::<hierarchy::Entity, hierarchy::Model, hierarchy::ActiveModel>()
+        .await?;
+
+    debug!("{:?}", Json(&hierarchies));
+    Ok((StatusCode::OK, Json(hierarchies)).into_response())
+}
+
+pub async fn create_hierarchy(
+    State(app_state): State<AppState>,
+    Json(hierarchy): Json<CreateBasicLookUpDTO>,
+) -> Result<Response> {
+    let active_model = hierarchy::ActiveModel {
+        id: Default::default(),
+        name: sea_orm::Set(hierarchy.name),
+    };
+
+    app_state
+        .lookup_repository
+        .create::<hierarchy::Entity, hierarchy::Model, hierarchy::ActiveModel>(active_model)
+        .await?;
+
+    Ok(StatusCode::CREATED.into_response())
 }
--- infrastructure/web/src/routes/lookup.rs
+++ infrastructure/web/src/routes/lookup.rs
@@ -4,7 +4,10 @@ use axum::{
 };
 
 use crate::{
-    controllers::lookup::{create_brigade, get_brigades},
+    controllers::lookup::{
+        create_brigade, create_charge, create_division, create_hierarchy, create_organism,
+        get_brigades, get_charges, get_divisions, get_hierarchies, get_organisms,
+    },
     state::AppState,
 };
 use std::sync::Arc;
@@ -13,5 +16,13 @@ pub fn lookup_routes(app_state: &Arc<AppState>) -> Router {
     Router::new()
         .route("/lookup/brigade", get(get_brigades))
         .route("/lookup/brigade", post(create_brigade))
+        .route("/lookup/organism", get(get_organisms))
+        .route("/lookup/organism", post(create_organism))
+        .route("/lookup/division", get(get_divisions))
+        .route("/lookup/division", post(create_division))
+        .route("/lookup/charge", get(get_charges))
+        .route("/lookup/charge", post(create_charge))
+        .route("/lookup/hierarchy", get(get_hierarchies))
+        .route("/lookup/hierarchy", post(create_hierarchy))
         .with_state(app_state.as_ref().clone())
 }
